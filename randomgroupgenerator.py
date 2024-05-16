81a25e7d9f0507052fc47ed70b901898b7473cf1
Siming Chen <143737200+midnightmoonrise@users.noreply.github.com>
2024-05-16 19:24:15 -0400

    Custom people list size.

--- randomgroupgenerator.py
+++ randomgroupgenerator.py
@@ -3,20 +3,22 @@ import random
 def main():
     people = []
     groupnumber = 0
-    while (groupnumber < 1 or groupnumber > 16):
-        groupnumber = int(input("Enter number of groups: "))
+    peoplecount = 0
     with open("randomgroupgenpeople.txt", "r") as file:
         for line in file:
             people.append(line.rstrip())
+            peoplecount += 1
+    while (groupnumber < 1 or groupnumber > peoplecount):
+        groupnumber = int(input("Enter number of groups: "))
     peopleingroup = []
-    smallestgroup = 16 // groupnumber
+    smallestgroup = peoplecount // groupnumber
     for i in range(groupnumber):
-        if i < 16 % groupnumber:
+        if i < peoplecount % groupnumber:
             peopleingroup.append(smallestgroup + 1)
         else:
             peopleingroup.append(smallestgroup)
     # create a 2D array, where the rows contain groupnumber - 1 lists of people.
-    groups = [["placehold" for x in range(16 // groupnumber)] for y in range(groupnumber - 1)]
+    groups = [["placehold" for x in range(peoplecount // groupnumber)] for y in range(groupnumber - 1)]
     for i in range(groupnumber - 1): #remember, there are only groupnumber - 1 rows.
         # if the group is large, add another place.
         if peopleingroup[i] > smallestgroup:
